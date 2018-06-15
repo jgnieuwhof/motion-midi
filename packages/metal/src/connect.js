e54827642875159652f490847184bb3950035d5e
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-14 20:29:54 -0400

    :tada: working binary stream

--- packages/metal/src/connect.js
+++ packages/metal/src/connect.js
@@ -1,7 +1,11 @@
 import SerialPort from "serialport";
+const ByteLength = require("@serialport/parser-byte-length");
 
 import { baudRate } from "./common/env";
 
-const connect = port => new SerialPort(port.comName, { baudRate });
+const connect = port =>
+  new SerialPort(port.comName, { baudRate }).pipe(
+    new ByteLength({ length: 5 })
+  );
 
 export default connect;
--- packages/metal/src/index.js
+++ packages/metal/src/index.js
@@ -1,23 +1,28 @@
+import { merge, padStart, set } from "lodash";
+import { Parser } from "binary-parser";
+
 import connect from "./connect";
 import listPorts from "./listPorts";
 
-// function getConnectedArduino() {
-//   var serialPort = require("serialport");
-//   sp = new serialPort(arduinoport, {
-//     buadRate: 9600
-//   });
-//   sp.on("open", function() {
-//     console.log("done! arduino is now connected at port: " + arduinoport);
-//   });
-// }
+const parser = new Parser()
+  .endianess("little")
+  .uint8("id")
+  .uint32("duration");
+
+let state = [];
 
 const main = async () => {
   const ports = await listPorts();
   if (ports.length) {
     const port = connect(ports[0]);
+    console.log(`Connected to: ${ports[0].comName}`);
     port.on("data", chunk => {
-      console.log(chunk.toString());
+      console.log(parser.parse(chunk));
     });
+
+    // setInterval(() => {
+    //   console.log(JSON.stringify(state));
+    // }, 1000 / 10);
   }
 };
 
