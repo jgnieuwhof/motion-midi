5dd6169b5983f1f3ef136d3c681cc0e40ed0d907
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-17 04:00:15 -0400

    :bug: tried & tested, seems to work

--- packages/server/src/sockets/streamSensorData.js
+++ packages/server/src/sockets/streamSensorData.js
@@ -1,38 +1,49 @@
-import { mapObjIndexed, median } from 'ramda';
+import { median } from 'ramda';
 import socketStream from 'socket.io-stream';
 
-import { listen, parser } from '@motion-midi/util';
+import { listen, parser, squash } from '@motion-midi/util';
+
+import { socketName } from '../env';
 
 const createStream = ({ fps, window } = {}) => {
   let durations = {};
-  const stream = new Readable({ read: () => {} });
-
-  listen({
-    socket: socketName,
-    callback: data => {
-      const { id, duration } = parser.parse(data);
-      if (!durations[id]) {
-        durations[id] = [duration];
-      } else {
-        durations[id] = [
-          ...durations[id].slice(durations[id].length === window ? 1 : 0),
-          duration,
-        ];
-      }
-    },
+  const stream = socketStream.createStream({
+    allowHalfOpen: true,
   });
 
-  setInterval(() => {
-    stream.push(mapObjIndexed(d => median(d)));
+  const client = listen({ socket: socketName });
+
+  client.on('data', data => {
+    const { id, duration } = parser.parse(data);
+    if (!durations[id]) {
+      durations[id] = [duration];
+    } else {
+      durations[id] = [
+        ...durations[id].slice(durations[id].length === window ? 1 : 0),
+        duration,
+      ];
+    }
+  });
+
+  const interval = setInterval(() => {
+    Object.keys(durations).forEach(id =>
+      stream.write(squash({ id, duration: median(durations[id]) })),
+    );
   }, 1000 / fps);
 
-  return stream;
+  return {
+    stream,
+    close: () => {
+      client.destroy();
+      stream.destroy();
+      clearInterval(interval);
+    },
+  };
 };
 
 export default ({ socket }) =>
-  socketStream(socket).on(
-    `client::stream`,
-    (stream, { fps = 10, window = 5 } = {}) => {
-      createStream({ fps, window }).pipe(stream);
-    },
-  );
+  socket.on('client::stream', ({ fps = 10, window = 5 }) => {
+    const { stream, close } = createStream({ fps, window });
+    socketStream(socket).emit('server::stream', stream);
+    socket.on('client::streamEnd', close);
+  });
